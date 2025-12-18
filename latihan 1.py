1bd56f28d24e68b194d15ec8717c297adceaf768
Bila <nabilafawwaz57@gmail.com>
2025-12-18 08:39:20 +0700

    Menambahkan File

---
+++ latihan 1.py
@@ -0,0 +1,15 @@
+txt = 'Hello World'
+
+print(len(txt))
+
+print(txt[-1])
+
+print(txt[2:5])
+
+print(txt.replace(" ", ""))
+
+print(txt.upper())
+
+print(txt.lower())
+
+print(txt.replace("H", "J"))
---
+++ latihan 2.py
@@ -0,0 +1,4 @@
+umur = 17
+txt = 'Hello, nama saya bila, dan umur saya adalah {} tahun'
+
+print(txt.format(umur))
